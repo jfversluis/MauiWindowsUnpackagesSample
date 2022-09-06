e0ee69acb81636c2098b8b989578ef1f83fb82a1
Gerald Versluis <gerald@verslu.is>
2022-09-06 09:06:11 +0200

    Initial commit

---
+++ MauiWindowsUnpackagesSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiWindowsUnpackagesSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MauiWindowsUnpackagesSample
+ Sample code to demonstrate how to build and distribute a so-called unpackaged Windows app with .NET MAUI
